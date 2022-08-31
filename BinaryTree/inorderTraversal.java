08adc5fbf9aa8575437cd2fc1d0582a423887682
Kritartha <68478501+Kritartha1@users.noreply.github.com>
2022-08-31 19:59:04 +0530

    Update inorderTraversal.java

--- BinaryTree/inorderTraversal.java
+++ BinaryTree/inorderTraversal.java
@@ -42,3 +42,45 @@ class Solution {
     }
 }
 
+
+//Morris Traversal
+
+// Step 1: Initialize current as root
+
+// Step 2: While current is not NULL,
+
+// If current does not have left child
+
+//     a. Add current’s value
+
+//     b. Go to the right, i.e., current = current.right
+
+// Else
+
+//     a. In current's left subtree, make current the right child of the rightmost node
+
+//     b. Go to this left child, i.e., current = current.left
+    
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> ans=new LinkedList<>();
+        TreeNode curr=root;
+        TreeNode pre;
+        while(curr!=null){
+            if(curr.left==null){
+                ans.add(curr.val);
+                curr=curr.right;
+            }else{
+                pre=curr.left;
+                while(pre.right!=null){
+                    pre=pre.right;
+                }
+                pre.right=curr;
+                TreeNode temp=curr;
+                curr=curr.left;
+                temp.left=null;
+            }
+        }
+        return ans;
+    }
+}
